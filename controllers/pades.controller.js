23348b4d641c61e8c38a6907eb87c47e0ba43b1b
jupa1986 <jupa1986@jupa1986s-MacBook-Pro.local>
2019-05-14 17:45:19 -0400

    cambio a libreria java

--- controllers/pades.controller.js
+++ controllers/pades.controller.js
@@ -1,5 +1,6 @@
-const pdfsig = require('pdfsig');
+const firmaDigitalPdf = require('validar-firma-digital-pdf');
 const { crudController } = require('mini-mvcs');
+const fs = require('fs');
 
 module.exports = (router, services) => {
 
@@ -7,16 +8,37 @@ module.exports = (router, services) => {
   *
   * @return {String} Devuelve el estado del servicio
   */
-  router.get('/estado', (req, res) => {
-    return {
-      status: 'El servicio se encuentra disponible',
-    };
 
+
+  router.get('/test/firmas', (req, res) => {
+    let documento = base64_encode();
+		console.log("TCL: documento", documento)
+    
+    return firmaDigitalPdf.obtenerFirmasBase64(documento.toString(), true);
   });
 
-  router.get('/firma', (req, res) => {
-    const firmas = new pdfsig('./test/prueba.pdf');
-    return firmas;
+  router.get('/test/base64Ejemplo', (req, res) => {
+    let documento = base64_encode();
+    return {documento};
+  });
 
+  router.post('/firmas', (req, res) => {
+    if (req.query.formato = 'base64'){
+      const documento = req.body.documento;
+      return firmaDigitalPdf.obtenerFirmasBase64(documento.toString(), true);
+    } 
+    return {
+      mensaje: "Formato Desconocido"
+    };
+    
+    
   });
 };
+
+const base64_encode = (file) => {
+  // read binary data
+  var bitmap = fs.readFileSync(`./test/prueba.pdf`, 'utf8');
+  return new Buffer(bitmap).toString('base64');
+  // convert binary data to base64 encoded string
+  //return bitmap;
+}
