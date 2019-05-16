fb2e0ffd304e818cbb019ccfc138aba9ead9cee1
jupa1986 <jupa1986@jupa1986s-MacBook-Pro.local>
2019-05-16 08:54:04 -0400

    configuracion de ejemplo

---
+++ config/config.example.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    server: {
+      port: 4000,
+    },
+    api: {
+      main: '/api/v1/',
+    },
+  },
+  test: {
+    server: {
+      port: 4000,
+    },
+  },
+  production: {
+    server: {
+      port: 4000,
+    },
+    api: {
+      main: '/api/v1/',
+    },
+  },
+};
+
